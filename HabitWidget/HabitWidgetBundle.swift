e7411ef1d119c00e5cdfc578b095208fca0e3991
rotartechspaceapp <123818046+nikolay1414@users.noreply.github.com>
2025-12-08 23:31:36 +0300

    Add widget

---
+++ HabitWidget/HabitWidgetBundle.swift
@@ -0,0 +1,12 @@
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HabitWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HabitWidget()
+        HabitWidgetControl()
+        HabitWidgetLiveActivity()
+    }
+}
